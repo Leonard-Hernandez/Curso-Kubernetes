45257e7db8b0f7e6ba2a92bf6b3755adfb88d1ee
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-10-03 11:14:54 -0500

    ADD: repositorio del usuario

---
+++ msvc-usuarios/src/main/java/org/leonard/springcloud/msvc/usuarios/msvc_usuarios/repositories/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package org.leonard.springcloud.msvc.usuarios.msvc_usuarios.repositories;
+
+import org.leonard.springcloud.msvc.usuarios.msvc_usuarios.models.entity.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+}
